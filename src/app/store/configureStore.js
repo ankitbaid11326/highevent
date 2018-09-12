1b82fe19ad279da2aeeab05558f397f2e3041411
Ankit Baid Home PC <ankitbaid11326@gmail.com>
2018-09-13 00:32:27 +0530

    Redux Integration

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers/rootReducers';
+
+export const configureStore = preloadedState => {
+    const middlewares = [];
+    const middlewareEnhancer = applyMiddleware(...middlewares);
+
+    const storeEnhancers = [middlewareEnhancer];
+
+    const composedEnhancer = composeWithDevTools(...storeEnhancers);
+
+    const store = createStore(rootReducer, preloadedState, composedEnhancer);
+
+    if (process.env.NODE_ENV !== 'production') {
+        if (module.hot) {
+            module.hot.accept('../reducers/rootReducers', () => {
+                const newRootReducer = require('../reducers/rootReducers').default;
+                store.replaceReducer(newRootReducer);
+            });
+        }
+    }
+
+    return store;
+};
